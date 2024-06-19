2e5cec2ca9058501ea32db2a1be3dc225ed5bf7b
ptejalaxioned <tejalp@axioned.com>
2024-06-19 12:48:19 +0530

    JS File corection commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,19 @@ $(document).ready(function () {
   $("a").click(function () {
     let list = this.parentNode;
     if (list.children[3].innerHTML === "Read More") {
-      $(list.children[2].lastElementChild).show(1000);
       $(list).addClass("li-read-more");
       $(list.children[3]).addClass("read-less");
+      $(list.children[3]).removeClass("a-padding");
       list.children[3].innerHTML ="Read Less"
-      $('.dots').hide()
+      $(list.children[2].lastElementChild).show(1000);
+      $('.dots').hide(1000)
     } else {
-      $(list.children[2].lastElementChild).hide(1000);
       $(list).removeClass("li-read-more");
       $(list.children[3]).removeClass("read-less");
+      $(list.children[3]).addClass("a-padding");
       list.children[3].innerHTML ="Read More"
+      $(list.children[2].lastElementChild).hide(1000);
+      $('.dots').show(1000)
     }
   });
 });
