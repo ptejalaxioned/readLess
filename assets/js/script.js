992aca9a45b95903d5cd79c66a7e93fd4cb6463e
ptejalaxioned <tejalp@axioned.com>
2024-06-19 11:47:14 +0530

    JS File commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function () {
+  $(".more-text").hide();
+  $("a").click(function () {
+    let list = this.parentNode;
+    if (list.children[3].innerHTML === "Read More") {
+      $(list.children[2].lastElementChild).show(1000);
+      $(list).addClass("li-read-more");
+      $(list.children[3]).addClass("read-less");
+      list.children[3].innerHTML ="Read Less"
+    } else {
+      $(list.children[2].lastElementChild).hide(1000);
+      $(list).removeClass("li-read-more");
+      $(list.children[3]).removeClass("read-less");
+      list.children[3].innerHTML ="Read More"
+    }
+  });
+});
